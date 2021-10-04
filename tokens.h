04076b019c65a1fd7ec0a0f3c6383b58076e34c5
Michael15437 <michaelael@hotmail.com>
2021-10-04 16:43:23 +0300

    Create tokens.h

---
+++ tokens.h
@@ -0,0 +1,43 @@
+/*
+ */
+
+#ifndef TOKENS_H_INCLUDED
+#define TOKENS_H_INCLUDED
+
+/* Defining the tokens with numbers */
+
+#define IF 0
+#define ELSE 1
+#define INT 2
+#define RETURN 3
+#define VOID 4
+#define WHILE 5
+#define PLUS 6
+#define MINUS 7
+#define TIMES 8
+#define DIVIDE 9
+#define GREAT 10
+#define GREATEQ 11
+#define SMALL 12
+#define SMALLEQ 13
+#define EQUALITY 14
+#define NOTEQ 15
+#define EQUAL 16
+#define QUESTIONM 17
+#define COMMA 18
+#define LPAR 19
+#define RPAR 20
+#define LBRAC 21
+#define RBRAC 22
+#define LCURLY 23
+#define RCURLY 24
+#define LCOMM 25
+#define RCOMM 26
+#define T_ID 27
+#define T_NUM 28
+#define T_letter 29
+#define T_digit 30
+#define T_white 31
+
+#endif
+
